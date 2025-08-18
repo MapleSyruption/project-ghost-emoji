c3d74e8cdf3ffe54249fb12b4b1fd5b238af5339
Jordan <JordanH.Eggleston@gmail.com>
2025-08-18 15:27:35 -0400

    Text that says hi

---
+++ project-ghost-emoji/Assets/AScriptThatDoesNothing.cs
@@ -0,0 +1,22 @@
+using UnityEngine;
+
+public class AScriptThatDoesNothing : MonoBehaviour
+{
+
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        // This code does nothing
+        // EXCEPT for printing a line to the console
+        Debug.Log("This is a harmless line of code");
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        
+    }
+}
