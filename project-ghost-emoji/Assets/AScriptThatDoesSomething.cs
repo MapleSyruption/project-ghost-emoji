5d3e2431f334ca719ec25449ae2e2d972de3fee4
Jordan <JordanH.Eggleston@gmail.com>
2025-09-30 19:15:29 -0400

    Update AScriptThatDoesSomething.cs

--- project-ghost-emoji/Assets/AScriptThatDoesSomething.cs
+++ project-ghost-emoji/Assets/AScriptThatDoesSomething.cs
@@ -5,10 +5,12 @@ public class AScriptThatDoesSomething : MonoBehaviour
 
     public bool isBlue;
     public TMPro.TextMeshPro text;
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-
+        // I want the text to start red,
+        // so I set "isBlue" to false
         isBlue = false;
         
     }
@@ -16,11 +18,9 @@ public class AScriptThatDoesSomething : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (!isBlue)
+        if (isBlue == false)
         {
             text.color = Color.blue;
-            
-
         } else
         {
             text.color = Color.red;
