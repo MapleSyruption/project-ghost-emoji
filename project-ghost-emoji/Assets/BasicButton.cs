17df4244d1c3a574d0ffc69a0bb9814f94e7b580
Madeleine <madeleineburdzy@gmail.com>
2025-11-04 18:49:59 -0500

    textone Bye

--- project-ghost-emoji/Assets/BasicButton.cs
+++ project-ghost-emoji/Assets/BasicButton.cs
@@ -2,7 +2,13 @@ using UnityEngine;
 
 public class BasicButton : MonoBehaviour
 {
-    public string textone;
+    public string textone; public TMPro.TextMeshPro textToChange;
+    public void Display ()
+    {
+        Debug.Log("it got this far"+ textToChange.text);
+        textToChange.text = textone;
+        Debug.Log("it got even farther"+ textToChange.text);
+    }
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
